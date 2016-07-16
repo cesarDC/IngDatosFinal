50b7d6052aa07d3d6f0b03c41bba3236878ed320
DilerxD <20131349@aloe.ulima.edu.pe>
2016-07-16 04:25:31 -0500

    loginDAO

--- src/ingdatos/grupo3/dao/LoginDAO.java
+++ src/ingdatos/grupo3/dao/LoginDAO.java
@@ -2,72 +2,67 @@ package ingdatos.grupo3.dao;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
-import ingdatos.grupo3.beans.CentroHospitalario;
+import ingdatos.grupo3.beans.Donante;
 import ingdatos.grupo3.beans.Usuario;
 import oracle.jdbc.internal.OracleTypes;
 
-public class LoginDAO{
-	String message=null;
-	
-	private Connection conectarse(){
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			return DriverManager.getConnection(
-					"jdbc:oracle:thin:@friccio.com:1521:XE", "usuario4",
-					"oracle");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	
-	public Usuario autenticarUsuario(Usuario user) {
-		
-		Connection conn = conectarse();
-		
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("execute ");
-			List<Profesor> profesores = new ArrayList<Profesor>();
-			while(rs.next()){
-				profesores.add(new Profesor(
-						rs.getInt("id"),
-						rs.getString("nombres"),
-						rs.getString("apellido_paterno"),
-						rs.getString("apellido_materno"),
-						rs.getString("dni"),
-						rs.getString("foto_url"),
-						rs.getInt("id_estudio"),
-						nombreEst(rs.getInt("id_estudio"))
-				));
-			}
-			conn.close();
-			return profesores;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			conn.close();
-			return null;
-		}
-	}
-
+public class LoginDAO {
+	String message;
+			
 	public String getMessage() {
 		return message;
 	}
 
+	
+	
+	
+	public Usuario getDonante(String nickname,String password) {
+		String msg;
+		Connection conn = ConexionDAO.conectarse();
+		CallableStatement cs=null;
+		
+		ResultSet rs=null;
+		String sql="{call PR_obtenerUsuario(?,?,?)}";
+		Usuario usuario=new Usuario();
+		try {
+			cs=conn.prepareCall(sql);			
+			cs.setString(1,nickname);	
+			cs.setString(2,password);
+			cs.registerOutParameter(3, OracleTypes.CURSOR);
+			cs.executeQuery();
+			rs = (ResultSet)cs.getObject(2);
+			if(rs.next()){
+				usuario.setIdUsuario(rs.getInt("idUsuario"));
+				usuario.setNombreUsuario(rs.getString("nombreUsuario"));
+				usuario.setPassword(rs.getString("password"));
+				usuario.setTipoEmpleado(rs.getString("tipoEmpleado"));
+				usuario.setIdLocal(rs.getInt("idLocal")); 
+			}else{
+				msg="Usuario no encontrado";
+			}
+		}catch (SQLException ex){
+			msg=ex.getMessage();
+		} finally {
+			try {
+				if(rs!=null){
+					rs.close();
+				}
+				if(cs !=null){
+					cs.close();
+				}
+				if(conn !=null){
+					conn.close();
+				}
+			} catch (SQLException ex){
+				msg=ex.getMessage();
+			}
+			
+		}
+		return usuario;
+	}
 }
